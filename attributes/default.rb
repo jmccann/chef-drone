d678f671da4298c353be207db052328ae055818c
Justin Campbell <justin@justincampbell.me>
2014-04-08 14:50:45 -0400

    Fix Rubocop line length

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,7 @@ default['drone']['temp_file']   = "/tmp/drone.deb"
 default['drone']['config_file'] = '/etc/init/drone.conf'
 default['drone']['address']     = '0.0.0.0'
 default['drone']['port']        = '80'
-default['drone']['droned_opts'] = "--port=#{node['drone']['address']}:#{node['drone']['port']}"
+default['drone']['droned_opts'] = [
+  "--port=#{node['drone']['address']}:#{node['drone']['port']}"
+].join(' ')
 default['drone']['drone_tmp']   = '/tmp/drone'
