ea8acc67d6f9a2b46b0865924249c677fb3c5b2f
Justin Campbell <justin@justincampbell.me>
2014-04-08 15:05:06 -0400

    Bump version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'justin@justincampbell.me'
 license          'Apache 2.0'
 description      'Installs/Configures Drone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 depends 'docker'
 
