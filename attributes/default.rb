8cca736a9d9b40ca2f970f182734629bd594c414
Ke Zhu <davidzhu.4.2@gmail.com>
2014-10-26 11:49:23 -0400

    correct default download url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['drone']['package_url'] = "http://downloads.drone.io/latest/drone.deb"
+default['drone']['package_url'] = "http://downloads.drone.io/master/drone.deb"
 default['drone']['temp_file'] = "/tmp/drone.deb"
 default['drone']['config_file'] = '/etc/init/drone.conf'
 default['drone']['droned_opts'] = '--port=:80'
