cee2bb412ffcb5c8b4dbe00b1876ee09a99ae264
Graham Weldon <graham.weldon@mail.rakuten.com>
2014-12-02 12:25:33 +0900

    Added TOML file attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default['drone']['temp_file'] = '/tmp/drone.deb'
 default['drone']['config_file'] = '/etc/init/drone.conf'
 default['drone']['droned_opts'] = ''
 default['drone']['drone_tmp'] = '/tmp/drone'
+default['drone']['toml_config_file'] = '/etc/drone/drone.toml'
 default['drone']['config'] = {
   'server' => {
     'port' => ':80',
