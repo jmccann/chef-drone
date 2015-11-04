50d29a3129db1fad9cbd7ae3f1558669b736af86
Petr Michalec <petr_michalec@cz.ibm.com>
2015-11-04 16:23:03 +0100

    bump patch version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'justin@justincampbell.me'
 license          'Apache 2.0'
 description      'Installs/Configures Drone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.5'
+version          '0.7.6'
 
 depends 'docker', '>= 2.0.0'
 depends 'ssl_certificate'
