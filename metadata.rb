55cb4933624e686ede7b0d173d88dd8953fc9f2b
Jacob McCann <jacob.mccann2@target.com>
2016-02-02 10:14:04 -0600

    Pin docker cookbook version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures Drone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.7.9'
 
-depends 'docker', '>= 2.2.2'
+depends 'docker', '~> 2.2.2'
 depends 'ssl_certificate'
 
 supports 'ubuntu', '>= 12.04'
