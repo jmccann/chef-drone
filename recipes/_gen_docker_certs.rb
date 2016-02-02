e55320f63808d4fd3b86e441ea7c34ebe0a37ae3
Jacob McCann <jacob.mccann2@target.com>
2016-02-02 10:13:19 -0600

    Split generation of docker certs to seperate helper recipe

---
+++ recipes/_gen_docker_certs.rb
@@ -0,0 +1,25 @@
+# drone, docker servers + ca
+server = ssl_certificate 'self-signed-server' do
+  namespace node['fqdn']
+  # NOTE, there is no longer :restart action on docker_service
+  # notifies :restart, 'docker_service[default]', :delayed
+  # notifies :restart, 'service[drone]', :delayed
+end
+
+node.set[node['drone']['server_crt']] = server.cert_path
+node.set[node['drone']['server_key']] = server.key_path
+
+# docker client
+client = ssl_certificate 'self-signed-docker-client' do
+  namespace "#{node['hostname']}-docker-client"
+  ca_cert_path server.ca_cert_path
+  ca_key_path server.ca_key_path
+  # NOTE, there is no longer :restart action on docker_service
+  # notifies :restart, 'docker_service[default]', :delayed
+end
+
+node.set['drone']['docker_tls_ca_crt'] || server.ca_cert_path
+node.set['drone']['docker_tls_server_crt'] || server.cert_path
+node.set['drone']['docker_tls_server_key'] || server.key_path
+node.set['drone']['docker_tls_client_crt'] || client.cert_path
+node.set['drone']['docker_tls_client_key'] || client.key_path
--- recipes/docker.rb
+++ recipes/docker.rb
@@ -1,61 +1,17 @@
+include_recipe 'drone::_gen_docker_certs' if node['drone']['generate_certs']
 
-# drone, docker servers + ca
-server = ssl_certificate 'self-signed-server' do
-  namespace node['fqdn']
-  # NOTE, there is no longer :restart action on docker_service
-  #notifies :restart, 'docker_service[default]', :delayed
-  #notifies :restart, 'service[drone]', :delayed
-  only_if node['drone']['generate_certs'].to_s
+docker_service 'default' do
+  version node['drone']['docker_version']
+  retries 1
+  retry_delay 20
+  host node['drone']['docker_hosts']
+  storage_driver node['drone']['docker_storage_driver']
+  tls node['drone']['docker_tls'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  tls_verify node['drone']['docker_tls_verify'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  tls_ca_cert node['drone']['docker_tls_ca_crt'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  tls_server_cert node['drone']['docker_tls_server_crt'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  tls_server_key node['drone']['docker_tls_server_key'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  tls_client_cert node['drone']['docker_tls_client_crt'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  tls_client_key node['drone']['docker_tls_client_key'] if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
+  action [:create, :start]
 end
-
-if node['drone']['generate_certs']
-  node.set[node['drone']['server_crt']] = server.cert_path
-  node.set[node['drone']['server_key']] = server.key_path
-end
-
-# docker client
-client = ssl_certificate 'self-signed-docker-client' do
-  namespace "#{node['hostname']}-docker-client"
-  ca_cert_path server.ca_cert_path
-  ca_key_path server.ca_key_path
-  # NOTE, there is no longer :restart action on docker_service
-  #notifies :restart, 'docker_service[default]', :delayed
-  only_if node['drone']['generate_certs'].to_s
-end
-
-if node['drone']['generate_certs']
-    node.set['drone']['docker_tls_ca_crt'] || server.ca_cert_path
-    node.set['drone']['docker_tls_server_crt'] || server.cert_path
-    node.set['drone']['docker_tls_server_key'] || server.key_path
-    node.set['drone']['docker_tls_client_crt'] || client.cert_path
-    node.set['drone']['docker_tls_client_key'] || client.key_path
-end
-
-
-if node['drone']['docker_tls'] && node['drone']['docker_tls_server_key']
-    docker_service 'default' do
-      version node['drone']['docker_version']
-      retries 1
-      retry_delay 20
-      host node['drone']['docker_hosts']
-      storage_driver node['drone']['docker_storage_driver']
-      tls node['drone']['docker_tls']
-      tls_verify node['drone']['docker_tls_verify']
-      tls_ca_cert node['drone']['docker_tls_ca_crt']
-      tls_server_cert node['drone']['docker_tls_server_crt']
-      tls_server_key node['drone']['docker_tls_server_key']
-      tls_client_cert node['drone']['docker_tls_client_crt']
-      tls_client_key node['drone']['docker_tls_client_key']
-      action [:create, :start]
-    end
-else
-    docker_service 'default' do
-      version node['drone']['docker_version']
-      retries 1
-      retry_delay 20
-      host node['drone']['docker_hosts']
-      storage_driver node['drone']['docker_storage_driver']
-      action [:create, :start]
-    end
-end
-
