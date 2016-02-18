0271ae65e7db98c24cc64cec306cbf50f0444c5a
Jacob McCann <jacob.mccann2@target.com>
2016-02-18 10:10:21 -0600

    Version bump docker cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures Drone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.7.9'
 
-depends 'docker', '~> 2.4.20'
+depends 'docker', '~> 2.5.3'
 depends 'ssl_certificate'
 
 supports 'ubuntu', '>= 12.04'
