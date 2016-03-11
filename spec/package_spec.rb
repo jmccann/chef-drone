cb15719fa53361f4cd63ec2939331394c94c4855
Jacob McCann <jacob.mccann2@target.com>
2016-03-11 08:19:44 -0600

    Add platform version for chefspec

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -8,7 +8,7 @@ require 'spec_helper'
 describe 'drone::package' do
   context 'When all attributes are default, on an unspecified platform' do
     cached(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu')
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
       runner.converge(described_recipe)
     end
 
