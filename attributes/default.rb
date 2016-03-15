e1b035794f57cd399e654c7d522d2f8c2018f5b3
Jacob McCann <jacob.mccann2@target.com>
2016-03-15 13:07:35 -0500

    Refactor setting Drone config

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,27 +24,18 @@ default['drone']['server_key'] = ''
 
 # database configuration
 #
-default['drone']['database_driver'] = 'sqlite3'
-default['drone']['database_config'] = '/var/lib/drone/drone.sqlite'
+default['drone']['config']['database_driver'] = 'sqlite3'
+default['drone']['config']['database_config'] = '/var/lib/drone/drone.sqlite'
 
-# oauth2 client/secret. REQUIRED
-#
-default['drone']['oauth_client'] = ''
-default['drone']['oauth_secret'] = ''
 # remote configuration
 #
-default['drone']['remote_driver'] = 'github'
-default['drone']['remote_config'] = 'https://github.com?client_id=${CLIENT}&client_secret=${SECRET}'
+default['drone']['config']['remote_driver'] = 'github'
+default['drone']['config']['remote_config'] = 'https://github.com?client_id=${CLIENT}&client_secret=${SECRET}'
 
 # docker configuration
-default['drone']['docker_hosts'] = ['unix:///var/run/docker.sock', 'unix:///var/run/docker.sock']
-
-# TODO, allow specify external docker or multiple docker socks
-# DOCKER_HOST_1=
-# DOCKER_CERT=""
-# DOCKER_KEY=""
-# DOCKER_CA=""
+default['drone']['config']['docker_host_1'] = 'unix:///var/run/docker.sock'
+default['drone']['config']['docker_host_2'] = 'unix:///var/run/docker.sock'
 
 # plugin configuration
 #
-default['drone']['plugin_filter'] = 'plugins/*'
+default['drone']['config']['plugin_filter'] = 'plugins/*'
