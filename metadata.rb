503003f8c5fdc17de6236fc297cfe6a3971e3bd0
Jacob McCann <jacob.mccann2@target.com>
2016-06-14 13:02:27 -0500

    Update git maintainer and source info

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name             'drone'
-maintainer       'Justin Campbell'
-maintainer_email 'justin@justincampbell.me'
+maintainer       'Jacob McCann'
+maintainer_email 'jmccann.git@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures Drone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url       'https://github.com/justincampbell/chef-drone'
-issues_url       'https://github.com/justincampbell/chef-drone/issues'
+source_url       'https://github.com/jmccann/chef-drone'
+issues_url       'https://github.com/jmccann/chef-drone/issues'
 version          '1.0.0'
 
 depends 'chef-vault', '~> 1.3'
