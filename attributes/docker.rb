f831735da8e61c3ab1e0090db5cce2930aab2325
Austin Heiman <atheimanksu@gmail.com>
2016-07-01 07:52:24 -0500

    added centos support

--- attributes/docker.rb
+++ attributes/docker.rb
@@ -2,10 +2,17 @@
 # NOTE, assuming to switch from default['drone']['docker_*'] to default['docker'][....]
 
 default['drone']['docker']['version'] = '1.9.1'
-default['drone']['docker']['storage_driver'] = 'aufs'
 default['drone']['docker']['hosts'] = ['unix:///var/run/docker.sock']
 default['drone']['docker']['log_driver'] = nil
 default['drone']['docker']['log_opts'] = nil
+# select storage drive based on docker compatibility matrix
+# https://success.docker.com/Get_Help/Compatibility_Matrix_and_Maintenance_Lifecycle
+case node['platform']
+when 'debian', 'ubuntu'
+  default['drone']['docker']['storage_driver'] = 'aufs'
+when 'centos', 'redhat', 'amazon', 'scientific', 'oracle', 'fedora'
+  default['drone']['docker']['storage_driver'] = 'devicemapper'
+end
 
 default['drone']['docker']['tls'] = false
 default['drone']['docker']['tls_verify']  = false
