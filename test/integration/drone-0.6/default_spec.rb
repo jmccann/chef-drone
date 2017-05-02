f8c2870d9eef34983e25d039991f79a95bf625cf
Jacob McCann <jacob.mccann2@target.com>
2017-05-02 16:58:42 -0500

    Add initial Drone 0.6 testing

---
+++ test/integration/drone-0.6/default_spec.rb
@@ -0,0 +1,18 @@
+describe file('/var/lib/drone/drone.sqlite') do
+  it { should be_file }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+# Ubuntu image doesn't doesn't have cURL by default but wget can be used.
+describe command("wget -q --no-check-certificate http://localhost") do
+  its(:exit_status) { should eq 0 }
+end
+
+# No logs from agent (meaning no errors)
+describe command("docker logs agent") do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should include('pipeline: request next execution') }
+end
