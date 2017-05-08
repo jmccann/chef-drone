5957d28de9062a19a1d2e60d9b7355c4aa96574b
Jacob McCann <jacob.mccann2@target.com>
2017-05-08 09:24:13 -0500

    Remove no longer existent dependencies

--- metadata.rb
+++ metadata.rb
@@ -11,8 +11,6 @@ version          '4.0.0'
 
 depends 'chef-vault', '~> 1.3'
 depends 'docker', '~> 2.9'
-depends 'chef_nginx', '~> 6.0'
-depends 'ssl_certificate', '~> 1.12'
 
 supports 'ubuntu', '>= 16.04'
 supports 'centos', '>= 7.1'
