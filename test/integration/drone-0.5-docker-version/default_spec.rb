d24b006948fcb0caffc546c9928c9f4b1ac052b0
Jacob McCann <jacob.mccann2@target.com>
2017-05-24 12:47:50 -0500

    Add some tests

--- test/integration/drone-0.5-docker-version/default_spec.rb
+++ test/integration/drone-0.5-docker-version/default_spec.rb
@@ -16,3 +16,16 @@ describe command("docker logs agent") do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should include('connection established, ready to process builds') }
 end
+
+# Make sure correct docker version installed
+describe command("docker version") do
+  its(:stdout) { should include('Version:      17.05.0-ce') }
+end
+
+# Make sure running in host network mode
+describe command("docker inspect drone") do
+  its(:stdout) { should include('"NetworkMode": "host"') }
+end
+describe command("docker inspect agent") do
+  its(:stdout) { should include('"NetworkMode": "host"') }
+end
