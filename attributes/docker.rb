5ffc386f84d3b9d0d15f4f09a6185dc01e114503
Jordan Brockopp <Jordan.Brockopp@target.com>
2017-07-21 08:42:57 -0500

    add bridge ip attribute for setting bip

--- attributes/docker.rb
+++ attributes/docker.rb
@@ -1,5 +1,6 @@
 default['drone']['docker']['version'] = nil
 default['drone']['docker']['hosts'] = ['unix:///var/run/docker.sock']
+default['drone']['docker']['bridge_ip'] = nil
 default['drone']['docker']['cidr_v4'] = nil
 default['drone']['docker']['cidr_v6'] = nil
 default['drone']['docker']['log_driver'] = nil
